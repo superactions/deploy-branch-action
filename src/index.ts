d98d97b92990174a53d6962e295f793fedd9451d
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-07-12 14:08:21 +0200

    Init implementation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
-import { add } from './utils'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as artifact from '@superactions/artifact'
+import { createCommentOrUpdate } from '@superactions/comment'
+import { join } from 'path'
 
-// eslint-disable-next-line no-console
-console.log('Hello package-a!')
+const context = github.context
+const token = core.getInput('token')
+const artifactClient = artifact.create({ ghToken: token })
 
-export const something = add(2, 3)
+/**
+ * GitHub (Super) Action uploading a given directory as webpage and positing a comment with link back on PR.
+ */
+async function main(): Promise<void> {
+  const _directory = core.getInput('directory')
+  const directoryPath = join(process.env['GITHUB_WORKSPACE'] || process.cwd(), _directory)
+
+  if (context.eventName === 'pull_request') {
+    await artifactClient.uploadDirectory('deploy-branch-action', directoryPath)
+    const pageUrl = artifactClient.getPageUrl('deploy-branch-action')
+
+    await createCommentOrUpdate({
+      message: `Branch deployed at ${pageUrl}`,
+      uniqueAppId: 'superactions/deploy-branch-action',
+      githubToken: token,
+    })
+  }
+}
+
+main().catch((e: Error) => {
+  core.warning(e.stack!)
+  core.setFailed(e)
+})
--- src/utils/add.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'earljs'
-
-import { add } from './add'
-
-describe('test', () => {
-  it('works', () => {
-    expect(add(2, 2)).toEqual(4)
-  })
-})
